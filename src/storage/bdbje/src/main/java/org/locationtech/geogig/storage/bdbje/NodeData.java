152d289b066e7d4116b97784af7aad32c185c83c
Gabriel Roldan <gabriel.roldan@gmail.com>
2014-07-24 20:44:42 -0300

    add missing (c) headers, fix some old ones mentioning TOPP instead of OpenPlans

--- src/storage/bdbje/src/main/java/org/locationtech/geogig/storage/bdbje/NodeData.java
+++ src/storage/bdbje/src/main/java/org/locationtech/geogig/storage/bdbje/NodeData.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2013 OpenPlans. All rights reserved.
+ * This code is licensed under the BSD New License, available at the root
+ * application directory.
+ */
 package org.locationtech.geogig.storage.bdbje;
 
 import static org.locationtech.geogig.storage.GraphDatabase.SPARSE_FLAG;
--- src/web/api/src/main/java/org/locationtech/geogig/rest/repository/RepositoryRouter.java
+++ src/web/api/src/main/java/org/locationtech/geogig/rest/repository/RepositoryRouter.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2013 OpenPlans. All rights reserved.
+ * This code is licensed under the BSD New License, available at the root
+ * application directory.
+ */
 package org.locationtech.geogig.rest.repository;
 
 import org.locationtech.geogig.web.console.ConsoleResourceResource;
