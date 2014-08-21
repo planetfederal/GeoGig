e689f2e23854512f8a1f89ff2a76412af0402c19
Jody Garnett <jody.garnett@gmail.com>
2014-08-21 15:58:09 -0700

    Update headers for files created 2014 (#799)
    See #799

--- src/storage/bdbje/src/main/java/org/locationtech/geogig/storage/bdbje/NodeData.java
+++ src/storage/bdbje/src/main/java/org/locationtech/geogig/storage/bdbje/NodeData.java
@@ -1,7 +1,10 @@
-/* Copyright (c) 2013 OpenPlans. All rights reserved.
- * This code is licensed under the BSD New License, available at the root
- * application directory.
- */
+/*******************************************************************************
+ * Copyright (c) 2014 Boundless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *******************************************************************************/
 package org.locationtech.geogig.storage.bdbje;
 
 import static org.locationtech.geogig.storage.GraphDatabase.SPARSE_FLAG;
